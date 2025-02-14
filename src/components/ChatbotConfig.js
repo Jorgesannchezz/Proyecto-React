96f0c2e294e01a471a224921f07aa554d18d3668
Jorge Bolaños Sánchez <jorgebolanossanchez@alumno.ieselrincon.es>
2025-02-14 13:56:35 +0000

    Corrección en la importación de MessageParser4

--- src/components/ChatbotConfig.js
+++ src/components/ChatbotConfig.js
@@ -1,6 +1,6 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 import ActionProvider from './ActionProvider';
-import MessageParser from './MessageParser';
+import {MessageParser} from './MessageParser';
 
 const config = {
   botName: "GeminisBot",
